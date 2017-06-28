ee3a28f1b5be9def0071381a198105920e39b694
ChunTeWu <davidpypy@gmail.com>
2017-06-28 16:57:04 -0700

    Navigation done

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,25 @@
 // Insert JavaScript here...
+(function($){
+	'use strict';
+	const $header = $('header');
+	let $window = $(window);
+
+	console.log($window.width());
+
+	$('#mobile-menu').on('click', function(){
+		$header.toggleClass('show-mobile-menu');
+	});
+	$window.on('resize', function(){
+		if($window.width() <= 400){
+		console.log('sfd');
+		
+		$('#logo').attr('src', 'images/small_logo.jpg');
+	}else{
+		$('#logo').attr('src', 'images/logo.png');
+		}
+	});
+
+	
+
+
+})(jQuery);
