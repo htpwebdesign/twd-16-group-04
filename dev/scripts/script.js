9ea40cca6000ce053275b9772ab2bcd603de20f1
ChunTeWu <davidpypy@gmail.com>
2017-06-28 17:36:59 -0700

    Most of the things except media query font size and the popout box

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -4,18 +4,23 @@
 	const $header = $('header');
 	let $window = $(window);
 
-	console.log($window.width());
-
 	$('#mobile-menu').on('click', function(){
 		$header.toggleClass('show-mobile-menu');
 	});
 	$window.on('resize', function(){
+
+		if($window.width() <= 560){
+			$('#mussel').attr('src', 'images/mussel_small.jpg');
+
+		}else{
+			$('#mussel').attr('src', 'images/mussel.jpg');
+
+		}
+
 		if($window.width() <= 400){
-		console.log('sfd');
-		
-		$('#logo').attr('src', 'images/small_logo.jpg');
-	}else{
-		$('#logo').attr('src', 'images/logo.png');
+			$('#logo').attr('src', 'images/small_logo.jpg');
+		}else{
+			$('#logo').attr('src', 'images/logo.png');
 		}
 	});
 
